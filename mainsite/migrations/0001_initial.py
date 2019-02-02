d28303e9b26e0752734b5c8053a4a2ee8f48be3d
Leezhitian <2831135642@qq.com>
2019-02-02 19:16:25 +0800

    second commit

---
+++ mainsite/migrations/0001_initial.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.utils.timezone import utc
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('title', models.CharField(max_length=200)),
+                ('slug', models.CharField(max_length=200)),
+                ('body', models.TextField()),
+                ('pub_date', models.DateTimeField(default=datetime.datetime(2019, 2, 2, 7, 35, 45, 899444, tzinfo=utc))),
+            ],
+        ),
+    ]
